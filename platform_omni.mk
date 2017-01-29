b3f41c07015090fb6c794cf6772797eed3931852
Erik Castricum <git@cas-online.nl>
2017-01-29 16:59:42 +0100

    Revert "kitakami: introduce libshim"
    This reverts commit 36f68c30c3bfd7c7210b0a75116127c5835cefb8.

--- platform_omni.mk
+++ platform_omni.mk
@@ -20,7 +20,3 @@ include device/sony/kitakami-common/platform.mk
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab
-
-# Libshim
-PRODUCT_PACKAGES += \
-    libshim_camera
